334d03201170072d9d4a8c56b76f1c9462e7a1c8
Tejas Rote <tejas292002@gmail.com>
2023-06-28 22:09:07 +0530

    add: carousel --done

---
+++ src/components/Carousel/CustomCarousel.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/swiper-bundle.css";
+import "./customCarousel.css";
+import buildingData from "../../assets/dummyData/buildingData";
+import PropertyCard from "../PropertyCard";
+
+// Install Swiper modules
+SwiperCore.use([Navigation, Pagination, Autoplay]);
+
+const CustomCarousel = () => {
+  //TODO add propsp
+  const keyword = "";
+  const filteredBuildingData = buildingData.filter((item) =>
+    item.address.toLowerCase().includes(keyword.toLowerCase())
+  );
+
+  
+
+  return (
+    <div className="carousel-container">
+      <div className="carousel">
+        <div className="swiper-button-prev"></div>
+
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={5}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 1000, disableOnInteraction: false }}
+          loop={true}
+          effect="slide" // Set the desired transition effect
+          speed={1000} // Set the speed of the transition
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          {filteredBuildingData.map((building, index) => (
+            <SwiperSlide key={index}>
+              <PropertyCard
+                img={building.img}
+                price={building.price}
+                address={building.address}
+                bed={building.bed}
+                tub={building.tub}
+                area={building.area}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="swiper-button-next"></div>
+      </div>
+      <div className="swiper-pagination"></div>
+    </div>
+  );
+};
+
+export default CustomCarousel;
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -9,9 +9,14 @@ import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 import { faClock } from '@fortawesome/free-solid-svg-icons';
 import { faSackDollar } from '@fortawesome/free-solid-svg-icons';
 
+import CustomCarousel from '../Carousel/CustomCarousel';
+
+
 const Dashboard = () => {
-  // const filteredBuildingData = buildingData.filter((item) =>item.address.toLowerCase().includes(props.keyword.toLowerCase())
-  // );
+  //TODO change to props
+  const keyword = "";
+  const filteredBuildingData = buildingData.filter((item) => item.address.toLowerCase().includes(keyword.toLowerCase())
+  );
   const [active, setActive] = useState(0);
   const handleClick = (e) => {
     setActive(e);
@@ -26,25 +31,27 @@ const Dashboard = () => {
 
         <hr class="dashed-line"></hr>
 
-        <div className='dash-buttons'>
-          <button className={active == 1 ? "bookmark-button-click" : "bookmark-button"} onClick={() => { handleClick(1) }}>
-            Bookmarked
-            <span className='dash-icon'><FontAwesomeIcon icon={faBookmark} /></span></button>
+        <div className='dash-body'>
 
-          <button className={active == 2 ? "bookmark-button-click" : "bookmark-button"} onClick={() => { handleClick(2) }}>
-            Recently Viewed
-            <span className='dash-icon'><FontAwesomeIcon icon={faClock} /></span></button>
+          <div className='dash-buttons'>
+            <button className={active == 1 ? "bookmark-button-click" : "bookmark-button"} onClick={() => { handleClick(1) }}>
+              Bookmarked
+              <span className='dash-icon'><FontAwesomeIcon icon={faBookmark} /></span></button>
 
-          <button className={active == 3 ? "bookmark-button-click" : "bookmark-button"} onClick={() => { handleClick(3) }}>
-            Selling
-            <span className='dash-icon'><FontAwesomeIcon icon={faSackDollar} /></span></button>
+            <button className={active == 2 ? "bookmark-button-click" : "bookmark-button"} onClick={() => { handleClick(2) }}>
+              Recently Viewed
+              <span className='dash-icon'><FontAwesomeIcon icon={faClock} /></span></button>
+
+            <button className={active == 3 ? "bookmark-button-click" : "bookmark-button"} onClick={() => { handleClick(3) }}>
+              Selling
+              <span className='dash-icon'><FontAwesomeIcon icon={faSackDollar} /></span></button>
+          </div>
+
+          <div className='dash-carousel'>
+            <CustomCarousel />
+          </div>
+
         </div>
-
-        <div className='dash-carousel'>
-          {/* Carousel part comes here */}
-          {/* swiper and slick both libraries are already installed */}
-        </div>
-        
       </div>
     </section>
   )
