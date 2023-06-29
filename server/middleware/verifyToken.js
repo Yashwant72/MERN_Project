955b44b9f356a119e942a2b076fc70e66d8d4005
Yashwant Chavan <104508511+Yashwant72@users.noreply.github.com>
2023-06-29 13:35:51 +0530

    Add files via upload

---
+++ server/middleware/verifyToken.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+
+const verifyToken = (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(401).json({msg: "Not Authorized"})
+    }
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+        const token = req.headers.authorization.split(" ")[1]
+        jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
+            if (err) {
+                return res.status(403).json({msg: "Token Expired"})
+            }
+            else {
+                req.user = data
+                next()
+            }
+        })
+    }
+}
+module.exports = verifyToken
